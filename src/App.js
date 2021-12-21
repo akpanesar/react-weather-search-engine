ea3fb08c04c7eb6dbe4eb31c4e04889e61f78947
AKPanesar <88969540+akpanesar@users.noreply.github.com>
2021-12-21 20:19:17 +0000

    Weather and Footer components added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
+import "./Footer";
+import "./Weather";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <Weather />
+      <Footer />
+    </div>
+  );
 }
---
+++ src/Footer.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <p>
+      Coded by
+      <a
+        href="https://www.instagram.com/amanvalpanesar/"
+        target="_blank"
+        title="instagram Profile"
+        rel="noreferrer"
+      >
+        Amanpreet Panesar
+      </a>
+      open-sourced on
+      <a
+        href="https://github.com/akpanesar/react-weather-search-engine"
+        target="_blank"
+        title="Github Profile"
+        rel="noreferrer"
+      >
+        Github
+      </a>
+      hosted on
+      <a
+        href="https://www.netlify.com/"
+        target="_blank"
+        title="Netlify"
+        rel="noreferrer"
+      >
+        Netlify
+      </a>
+    </p>
+  );
+}
